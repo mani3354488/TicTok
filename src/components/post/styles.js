5392d8e2e8d8edd1e5f42be23dd0ce6ae2837f6f
mani3354488 <mani3354488@gmail.com>
2021-02-16 00:58:22 +0530

    Implemented snap flatlist for feed

--- src/components/post/styles.js
+++ src/components/post/styles.js
@@ -1,78 +1,78 @@
 import {StyleSheet, Dimensions} from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        width: '100%',
-        height: Dimensions.get('window').height - 24,
-    },
-    video: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-    },
-    uiButton: {
-        height: '100%',
-        justifyContent: 'flex-end',
-    },
-    verticalButton: {
-        alignSelf: 'flex-end',
-        height: 300,
-        justifyContent: 'space-between',
-        marginRight: 5
-    },
-    horizontalButton: {
-        padding: 10,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'flex-end'
-    },
-    handle: {
-        color: '#fff',
-        fontSize: 16,
-        fontWeight: '700',
-        marginBottom: 8
-    },
-    description: {
-        color: '#fff',
-        fontSize: 16,
-        fontWeight: '300',
-        marginBottom: 8
-    },
-    music: {
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    songName: {
-        color: '#fff',
-        fontSize: 16,
-        marginLeft: 5
-    },
-    songImage: {
-        width: 50,
-        height: 50,
-        borderRadius: 25,
-        borderWidth: 2,
-        borderColor: '#4c4c4c'
-    },
-    //vertical container
-    profilePicture: {
-        width: 50,
-        height: 50,
-        borderRadius: 25,
-        borderWidth: 2,
-        borderColor: '#fff'
-    },
-    verticalIcon: {
-        alignItems: 'center'
-    },
-    iconLabel: {
-       color: '#fff',
-       fontSize: 16,
-       fontWeight: '600',
-       marginTop: 2
-    }
+  container: {
+    width: '100%',
+    height: Dimensions.get('window').height - 24,
+  },
+  video: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+  uiButton: {
+    height: '100%',
+    justifyContent: 'flex-end',
+  },
+  verticalButton: {
+    alignSelf: 'flex-end',
+    height: 300,
+    justifyContent: 'space-between',
+    marginRight: 5,
+  },
+  horizontalButton: {
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+  },
+  handle: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '700',
+    marginBottom: 8,
+  },
+  description: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '300',
+    marginBottom: 8,
+  },
+  music: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  songName: {
+    color: '#fff',
+    fontSize: 16,
+    marginLeft: 5,
+  },
+  songImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: '#4c4c4c',
+  },
+  //vertical container
+  profilePicture: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: '#fff',
+  },
+  verticalIcon: {
+    alignItems: 'center',
+  },
+  iconLabel: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+    marginTop: 2,
+  },
 });
 
 export default styles;
