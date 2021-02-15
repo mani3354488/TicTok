d7fe43992957c343e249c9febeace9f65339d20a
mani3354488 <mani3354488@gmail.com>
2021-02-15 22:01:33 +0530

    styling for post component - done

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,18 +1,61 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, TouchableWithoutFeedback, Image } from 'react-native';
 import Video from 'react-native-video';
+import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 import styles from './styles';
 
+
 const Post = () => {
+    const [paused, setPaused] = useState(false);
+
+    const onPlayPausePress = () => {
+        setPaused(!paused);
+    };
     return (
         <View style={styles.container}>
-            <Video
-                source={{uri: 'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4?_=1'}}
-                style={styles.video}
-                onError={(e: LoadError) => console.log(e)}
-                resizeMode={'cover'}
-            />
+            <TouchableWithoutFeedback onPress={onPlayPausePress}>
+                <Video
+                    source={{uri: 'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4'}}
+                    style={styles.video}
+                    onError={(e: LoadError) => console.log(e)}
+                    resizeMode={'cover'}
+                    repeat={true}
+                    paused={paused}
+                />
+            </TouchableWithoutFeedback>
+            <View style={styles.uiButton}>
+                <View style={styles.verticalButton}>
+                    <Image style={styles.profilePicture} source={{uri:'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'}} />
+                    <View style={styles.verticalIcon}>
+                        <AntDesign name={'heart'} size={40} color="white" />
+                        <Text style={styles.iconLabel}>1010</Text>
+                    </View>
+                    <View style={styles.verticalIcon}>
+                        <FontAwesome name={'commenting'} size={40} color="white" />
+                        <Text style={styles.iconLabel}>comm</Text>
+                    </View>
+                    <View style={styles.verticalIcon}>
+                        <MaterialCommunityIcons name={'share'} size={40} color="white" />
+                        <Text style={styles.iconLabel}>share</Text>
+                    </View>
+                </View>
+                <View style={styles.horizontalButton}>
+                    <View>
+                        <Text style={styles.handle}>@manikantanath</Text>
+                        <Text style={styles.description}>Hey check this out!</Text>
+                        <View style={styles.music}>
+                            <Entypo name={'beamed-note'} size={24} color="white" />
+                            <Text style={styles.songName}>Aron Smith - Dancing'</Text>
+                        </View>
+                    </View>
+                    <Image style={styles.songImage} source={{uri:'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'}} />
+                </View>
+            </View>
         </View>
     );
 }
--- src/components/post/styles.js
+++ src/components/post/styles.js
@@ -3,7 +3,7 @@ import {StyleSheet, Dimensions} from 'react-native';
 const styles = StyleSheet.create({
     container: {
         width: '100%',
-        height: Dimensions.get('window').height,
+        height: Dimensions.get('window').height - 24,
     },
     video: {
         position: 'absolute',
@@ -11,6 +11,67 @@ const styles = StyleSheet.create({
         left: 0,
         bottom: 0,
         right: 0,
+    },
+    uiButton: {
+        height: '100%',
+        justifyContent: 'flex-end',
+    },
+    verticalButton: {
+        alignSelf: 'flex-end',
+        height: 300,
+        justifyContent: 'space-between',
+        marginRight: 5
+    },
+    horizontalButton: {
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end'
+    },
+    handle: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '700',
+        marginBottom: 8
+    },
+    description: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '300',
+        marginBottom: 8
+    },
+    music: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    songName: {
+        color: '#fff',
+        fontSize: 16,
+        marginLeft: 5
+    },
+    songImage: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        borderWidth: 2,
+        borderColor: '#4c4c4c'
+    },
+    //vertical container
+    profilePicture: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        borderWidth: 2,
+        borderColor: '#fff'
+    },
+    verticalIcon: {
+        alignItems: 'center'
+    },
+    iconLabel: {
+       color: '#fff',
+       fontSize: 16,
+       fontWeight: '600',
+       marginTop: 2
     }
 });
 
